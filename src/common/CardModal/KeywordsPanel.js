68adfbcc227528a4b89bf25f3d0d2050ac64c647
Travis Clark <travis.scott.clark@gmail.com>
2025-05-26 18:53:18 -0400

    Added a null check to weapon

--- src/common/CardModal/KeywordsPanel.js
+++ src/common/CardModal/KeywordsPanel.js
@@ -1,46 +1,48 @@
-import React from 'react';
+import React from "react";
 import {
   Divider,
   Typography,
   ExpansionPanel,
   ExpansionPanelSummary,
-  ExpansionPanelDetails
-} from '@material-ui/core';
-import { ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
-import keywords from 'constants/keywords';
+  ExpansionPanelDetails,
+} from "@material-ui/core";
+import { ExpandMore as ExpandMoreIcon } from "@material-ui/icons";
+import keywords from "constants/keywords";
 
-
-function drawKeyword(kw){
-
-  let keyword = kw
-  if(typeof keyword === "string"){
-    keyword = {name:kw}
+function drawKeyword(kw) {
+  let keyword = kw;
+  if (typeof keyword === "string") {
+    keyword = { name: kw };
   }
 
-  return(
-  <div key={keyword.name}>
-    <Typography variant="caption" color="textSecondary">
-      {keyword.name + (!keyword.value ? "" : (Number.isInteger(keyword.value) ? " " : ": " ) + keyword.value)}
-    </Typography>
-    <div style={{ flexGrow: 1 }} />
-    <Typography variant="body2">
-      {keyword.name in keywords ? keywords[keyword.name] : 'No definition found.'}
-    </Typography>
-    <Divider />
-  </div>)
-
+  return (
+    <div key={keyword.name}>
+      <Typography variant="caption" color="textSecondary">
+        {keyword.name +
+          (!keyword.value
+            ? ""
+            : (Number.isInteger(keyword.value) ? " " : ": ") + keyword.value)}
+      </Typography>
+      <div style={{ flexGrow: 1 }} />
+      <Typography variant="body2">
+        {keyword.name in keywords
+          ? keywords[keyword.name]
+          : "No definition found."}
+      </Typography>
+      <Divider />
+    </div>
+  );
 }
 
-function KeywordsPanel({ card, variant="inherit" }) {
-
+function KeywordsPanel({ card, variant = "inherit" }) {
   const cardKeywords = card.keywords;
 
   if (!(cardKeywords instanceof Array)) return null;
   else if (cardKeywords.length === 0) return null;
   const columnContainerStyles = {
-    display: 'flex',
-    flexDirection: 'column',
-    padding: '0px 24px 24px'
+    display: "flex",
+    flexDirection: "column",
+    padding: "0px 24px 24px",
   };
   return (
     <React.Fragment>
@@ -49,13 +51,13 @@ function KeywordsPanel({ card, variant="inherit" }) {
           <Typography variant={variant}>Keywords</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails style={columnContainerStyles}>
-          {cardKeywords?.map(kw =>drawKeyword(kw))}
+          {cardKeywords?.map((kw) => drawKeyword(kw))}
 
-          {card.weapons.map(w =>w.keywords?.map(kw=>drawKeyword(kw)))}
+          {card.weapons?.map((w) => w.keywords?.map((kw) => drawKeyword(kw)))}
         </ExpansionPanelDetails>
       </ExpansionPanel>
     </React.Fragment>
   );
-};
+}
 
 export default KeywordsPanel;
