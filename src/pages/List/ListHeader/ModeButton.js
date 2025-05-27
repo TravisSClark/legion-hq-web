177e763dadec6e531712a73fc28785c9e5a02b25
Travis Clark <travis.scott.clark@gmail.com>
2025-05-27 15:27:05 -0400

    Error on too many points

--- src/pages/List/ListHeader/ModeButton.js
+++ src/pages/List/ListHeader/ModeButton.js
@@ -1,16 +1,31 @@
-import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
-import { Button, ButtonBase, Chip, Icon, Menu, MenuItem, MenuList, Typography } from '@material-ui/core';
-import LargerTooltip from 'common/LargerTooltip';
-import legionModes from 'constants/legionModes';
-import DataContext from 'context/DataContext';
-import { ExpandMore } from '@material-ui/icons';
+import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
+import {
+  Button,
+  ButtonBase,
+  Chip,
+  Icon,
+  Menu,
+  MenuItem,
+  MenuList,
+  Typography,
+} from "@material-ui/core";
+import LargerTooltip from "common/LargerTooltip";
+import legionModes from "constants/legionModes";
+import DataContext from "context/DataContext";
+import { ExpandMore } from "@material-ui/icons";
 
-function ModeButton({ currentMode, points, maxPoints, tooltip, handleChangeMode }) {
+function ModeButton({
+  currentMode,
+  points,
+  maxPoints,
+  tooltip,
+  handleChangeMode,
+}) {
   const [anchorEl, setAnchorEl] = useState();
-  const handleOpenMenu = event => setAnchorEl(event.currentTarget);
+  const handleOpenMenu = (event) => setAnchorEl(event.currentTarget);
   const handleCloseMenu = () => setAnchorEl();
-  const {userSettings} = useContext(DataContext);
+  const { userSettings } = useContext(DataContext);
 
   const isOverPoints = points > maxPoints;
 
@@ -22,48 +37,74 @@ function ModeButton({ currentMode, points, maxPoints, tooltip, handleChangeMode 
         onClose={handleCloseMenu}
       >
         <MenuList>
-          { Object.getOwnPropertyNames(legionModes).map( modeName =>{
+          {Object.getOwnPropertyNames(legionModes).map((modeName) => {
             const mode = legionModes[modeName];
 
-            if(!userSettings.showStormTide && modeName.includes("storm tide"))
+            if (!userSettings.showStormTide && modeName.includes("storm tide"))
               return null;
 
-            return(
+            return (
               <MenuItem
-                key={"mn-"+modeName}
+                key={"mn-" + modeName}
                 selected={currentMode === modeName}
                 onClick={() => {
-                handleChangeMode(modeName);
-                handleCloseMenu();
-              }}
-            >{mode.name} ({mode.maxPoints} pts)</MenuItem>
+                  handleChangeMode(modeName);
+                  handleCloseMenu();
+                }}
+              >
+                {mode.name} ({mode.maxPoints} pts)
+              </MenuItem>
             );
-          })
-        }
+          })}
         </MenuList>
       </Menu>
-        <ButtonBase onClick={handleOpenMenu} >
-          <div style={{display:'flex', flexDirection:'column', alignItems:'center', backgroundColor:isOverPoints?'error':'transparent', borderRadius:10, borderWidth:1, paddingRight:7, paddingLeft:7, borderColor: isOverPoints ? 'transparent':'#aaa', borderStyle:'solid'}}>
-            <Typography variant='caption'>{points} / {maxPoints}</Typography>
-            <div style={{display:'flex', flex:0, flexDirection:'row', alignItems:'center'}}>
-              <Typography variant='caption' style={{fontSize:"0.5rem"}}>{legionModes[currentMode].name.toUpperCase()}</Typography>
-              <ExpandMore fontSize='small' sx={{ fontSize: 16 }}/>
-            </div>
+      <ButtonBase onClick={handleOpenMenu}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            backgroundColor: isOverPoints ? "#f44336" : "transparent",
+            borderRadius: 10,
+            borderWidth: 1,
+            paddingRight: 7,
+            paddingLeft: 7,
+            borderColor: isOverPoints ? "transparent" : "#aaa",
+            borderStyle: "solid",
+          }}
+        >
+          <Typography variant="caption">
+            {points} / {maxPoints}
+          </Typography>
+          <div
+            style={{
+              display: "flex",
+              flex: 0,
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="caption" style={{ fontSize: "0.5rem" }}>
+              {legionModes[currentMode].name.toUpperCase()}
+            </Typography>
+            <ExpandMore fontSize="small" sx={{ fontSize: 16 }} />
           </div>
-        </ButtonBase>
+        </div>
+      </ButtonBase>
     </React.Fragment>
   );
-};
+}
 
 ModeButton.defaultProps = {
-  tooltip: 'Toggle between Recon (600), Standard (1000), and Grand Army (1600) formats.'
+  tooltip:
+    "Toggle between Recon (600), Standard (1000), and Grand Army (1600) formats.",
 };
 
 ModeButton.propTypes = {
   tooltip: PropTypes.string,
   points: PropTypes.number.isRequired,
   maxPoints: PropTypes.number.isRequired,
-  handleChangeMode: PropTypes.func.isRequired
+  handleChangeMode: PropTypes.func.isRequired,
 };
 
 export default ModeButton;
